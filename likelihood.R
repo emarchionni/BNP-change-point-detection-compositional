3bd6ea6aa757781b3eb59472003fc4294143e48a
Edoardo Marchionni <edoardo.marchionni@gmail.com>
2022-11-05 16:53:35 +0100

    only transition densities cpp

--- likelihood.R
+++ likelihood.R
@@ -1,3 +1,24 @@
+#### LOG INTEGRATED LIKELIHOOD CLUSTER ####
+log_integrated_likelihood_cluster <- function(y, omega, n_clust, trunc){
+  
+  #'@param y: observation in the cluster / dim: time x components
+  #'@param omega: omega of cluster / dim: d
+  
+  if(n_clust == 1)
+    return(ddirichlet(y,omega, log = T))
+  
+  value <- ddirichlet(y[1,], omega, log = T)
+  
+  for(i in 2:n_clust)
+    value <- value + log_transition_densities(y[i-1, ], y[i, ], omega, trunc)
+  
+  
+  return(value)
+  
+}
+
+
+
 #### INITIALIZATION LIKELIHOOD ####
 
 full_log_integrated_likelihood <- function(y, rho, omega, n_clust, trunc){
